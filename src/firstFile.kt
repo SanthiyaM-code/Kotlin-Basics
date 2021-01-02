24723fbf2a4acc21104383e43b25d7ec05aa5bf9
SanthiyaM-code <santhiya10082001@gmail.com>
2021-01-02 09:47:14 +0530

    class and object in Kotlin

--- src/firstFile.kt
+++ src/firstFile.kt
@@ -1,6 +1,11 @@
 fun main(args :Array<String>)
 {
-    print("Hello world");
+    println("Hello world")
+    //object creation
+    var obj=Firstclass()
+
+    obj.name="First Kotlin class and objects"
+    println("The message from kotlin class is \"${obj.name}\"")
 
 }
 
