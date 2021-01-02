e9c0916f19ad8f00ff860ccb5ed61427af8c38cd
SanthiyaM-code <santhiya10082001@gmail.com>
2021-01-02 10:22:10 +0530

    if-else-expression

--- src/firstFile.kt
+++ src/firstFile.kt
@@ -1,8 +1,14 @@
 fun main(args :Array<String>)
 {
-   //Java class inside Kotlin
-    var objJava=JavaClass();
-    objJava.name="Santhiya";
-    print(objJava.name);
+   //if else expressions
+
+    var a:Int=5;
+
+
+    var result:String=if (a%2==0)
+        "even"
+    else
+        "odd"
+    print("$a is of type $result")
 }
 
