2f3e3c0da0d7e6030829557725bc32098e1cbdd5
SanthiyaM-code <santhiya10082001@gmail.com>
2021-01-21 20:00:01 +0530

    Mutable List in Kotlin

--- src/firstFile.kt
+++ src/firstFile.kt
@@ -1,14 +1,21 @@
 fun main(args:Array<String>)
 {
-    var values:List <Int> =  listOf<Int>(8,2,5,5,7,6)
+    var values:MutableList <Int> =  mutableListOf<Int>(8,2,5,5,7,6)
     for (i in values)
     {
         print("$i ")
     }
+    values.add(1)
+    values.add(2)
+    values.add(3)
+
+
     println(values[2])
     println(values.get(1))
     println(values.indexOf(5))
     println(values.lastIndexOf(5))
+    for (i in values)
+    {print("$i ")}
 
 
 
