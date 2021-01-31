56843a2e954ec24da4caddcd4d56c9528291c05a
SanthiyaM-code <santhiya10082001@gmail.com>
2021-01-31 13:18:55 +0530

    Scope function in Kotlin -let

--- src/firstFile.kt
+++ src/firstFile.kt
@@ -1,23 +1,17 @@
 import java.util.*
 
 //Scope Function
-//also- return context and context is it
-class Person
-{
-    var name:String=""
-    var dept:String="CSE"
-}
+//let- return context and context is it
+
 
 
 fun main()
 {
-    val p=Person().also{
-        it.name="Sandy"
-        it.dept="CSE"
-    }
-    with(p){
-        println(name)
-        println(dept)
+    val name:String?="Sandy"
+
+    name?.let {
+        println(it.reversed())
+        println(it.capitalize())
     }
 
 }
