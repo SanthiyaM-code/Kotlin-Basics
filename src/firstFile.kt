c674b913f69d168b21ce6e19275295ee4ea86ed1
SanthiyaM-code <santhiya10082001@gmail.com>
2021-02-01 21:04:48 +0530

    lateinit example

--- src/firstFile.kt
+++ src/firstFile.kt
@@ -1,18 +1,24 @@
 import java.util.*
-
 //Scope Function
 //let- return context and context is it
+class Student
+{
+    lateinit var name:String
+     fun Intoduction()
+     {
+         name="Santhiya"
+         println("My name is $name")
+     }
 
-
+}
 
 fun main()
 {
-    val name:String?="Sandy"
 
-    name?.let {
-        println(it.reversed())
-        println(it.capitalize())
-    }
+    var stu= Student()
+    stu.Intoduction()
+
+
 
 }
 
